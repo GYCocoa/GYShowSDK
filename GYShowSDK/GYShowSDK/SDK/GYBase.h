f9e91339f6a9baff84f5c8adb77fde26ee547af0
GYZ <401812221@qq.com>
2025-03-13 11:53:52 +0800

    第一版本SDK

---
+++ GYShowSDK/GYShowSDK/SDK/GYBase.h
@@ -0,0 +1,16 @@
+//
+//  GYBase.h
+//  GYShowSDK
+//
+//  Created by gyz on 2025/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GYBase : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GYShowSDK/GYShowSDK/SDK/GYBase.m
@@ -0,0 +1,12 @@
+//
+//  GYBase.m
+//  GYShowSDK
+//
+//  Created by gyz on 2025/3/13.
+//
+
+#import "GYBase.h"
+
+@implementation GYBase
+
+@end
